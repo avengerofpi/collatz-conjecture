015054d416f14d8ca7d4dfdf8ffaa08f2210b543
Cory Washington <tbd>
2022-07-01 19:48:48 -0400

    make modular analysis recursive
    Automatically explore progressively larger modulii without repeating residues
    that are already known to go down a path that dips below the starting point.

--- collatz_modularity.py
+++ collatz_modularity.py
@@ -18,29 +18,48 @@ def collatze_vector(a, b):
     else:
         return a // 2, b // 2
 
-def collatzModN(N):
-    for a in range(N):
-        print(f"Checking a = {a} (mod {N})")
+def collatzModN(N, residueList):
+    tooManyIters = list()
+    needsSplitting = list()
+    for A in residueList:
+        a = A
         b = N
+        print(f"Checking a = {a} (mod {b})")
         iters = 0
-        maxIters = 20
+        maxIters = 10
         while ((b >= N) and ((b % 2) == 0) and (iters < maxIters)):
             iters += 1
             print(f"  {a:>4d} + {b:>4d}k")
             a, b = collatze_vector(a, b)
         print(f"  {a:>4d} + {b:>4d}k")
 
-        # Report conclusions
+        # Report conclusions, checking each of the loop-exiting conditions
         if (b < N):
-            print(f"  Decreases    ((b={b} <  (N={N}))")
+            print(f"  Decreases       ((b={b}) <(N={N}))")
+        elif ((b % 2) == 1):
+            print(f"  Needs splitting ((b={b} is odd))")
+            needsSplitting.append(A)
+        elif (iters >= maxIters):
+            print(f"  Needs more iters (current maxIters is {maxIters})")
+            tooManyIters.append(A)
         else:
-            print(f"  Inconclusive ((b={b} >= (N={N}))")
-            if ((b % 2) == 1):
-                print(f"  Needs splitting (b={b} is odd)")
-            elif (iters >= maxIters):
-                print(f"  Needs more iters (current maxIters is {maxIters})")
+            raise Exception("Error! Should not have reached this point")
 
+    if (len(tooManyIters) > 0):
+        print()
+        print(f"There are entries that required more iters than were allowed (maxIters was {maxIters})")
+        return
+    else:
+        return needsSplitting
 
 # Main routine
-N = 32
-collatzModN(N)
+N = 2
+needsSplitting = collatzModN(N, range(N))
+while needsSplitting:
+    print("-"*50)
+    print(f"needsSplitting for N = {N}: {needsSplitting}")
+    splitInput = sorted([ x for xx in [[ a, a + N ] for a in needsSplitting] for x in xx])
+    N *= 2
+    print(f"splitInput     for N = {N}: {splitInput}")
+    needsSplitting = collatzModN(N, splitInput)
+    nLog2 += 1
