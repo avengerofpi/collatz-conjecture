4829db18a5039fa568be990a58d7d341cc91e61f
Cory Washington <tbd>
2022-07-05 16:23:27 -0400

    Update output for modular collatz
    Output is now of the form:
    --------------------------------------------------
       2 residues to check          for N = 2: range(0, 2)
    Checking a =  0 = 0b 0 (mod  2) ->   0 +  1k   Decreases       ((b= 1) < (N=2))
    Checking a =  1 = 0b 1 (mod  2) ->   2 +  3k   Needs splitting ((b= 3 is odd))
       1 residues needing splitting for N = 2: [1]
    --------------------------------------------------
       2 residues to check          for N = 4: [1, 3]
    Checking a =  1 = 0b  1 (mod  4) ->   1 +  3k   Decreases       ((b= 3) < (N=4))
    Checking a =  3 = 0b 11 (mod  4) ->   8 +  9k   Needs splitting ((b= 9 is odd))
       1 residues needing splitting for N = 4: [3]
    --------------------------------------------------
       2 residues to check          for N = 8: [3, 7]
    Checking a =  3 = 0b  11 (mod  8) ->   4 +  9k   Needs splitting ((b= 9 is odd))
    Checking a =  7 = 0b 111 (mod  8) ->  26 + 27k   Needs splitting ((b=27 is odd))
       2 residues needing splitting for N = 8: [3, 7]
      NO ADDITIONAL PROGRESS FOR N = 8 (2 ** 3)
    --------------------------------------------------
       4 residues to check          for N = 16: [3, 7, 11, 15]
    Checking a =   3 = 0b   11 (mod  16) ->    2 +   9k   Decreases       ((b=  9) < (N=16))
    Checking a =   7 = 0b  111 (mod  16) ->   13 +  27k   Needs splitting ((b= 27 is odd))
    Checking a =  11 = 0b 1011 (mod  16) ->   20 +  27k   Needs splitting ((b= 27 is odd))
    Checking a =  15 = 0b 1111 (mod  16) ->   80 +  81k   Needs splitting ((b= 81 is odd))
       3 residues needing splitting for N = 16: [7, 11, 15]

--- collatz_modularity.py
+++ collatz_modularity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 # Imports
+from math import log10, log2
 
 # Functions
 def collatz(n):
@@ -19,25 +20,29 @@ def collatze_vector(a, b):
         return a // 2, b // 2
 
 def collatzModN(N, residueList):
+    maxResidueWidth=int(log10(max(residueList))) + 2
+    maxResidueWidthBinary=int(log2(max(residueList))) + 2
+    maxModulusWidth=int(log10(N)) + 2
     tooManyIters = list()
     needsSplitting = list()
     for A in residueList:
         a = A
         b = N
-        print(f"Checking a = {a} (mod {b})")
+        print(f"Checking a = {a:>{maxResidueWidth}d} = 0b{a:>{maxResidueWidthBinary}b} (mod {b:>{maxModulusWidth}d}) -> ", end="")
         iters = 0
+        maxIters = 30
         maxIters = 10
         while ((b >= N) and ((b % 2) == 0) and (iters < maxIters)):
             iters += 1
-            print(f"  {a:>4d} + {b:>4d}k")
+            #print(f"  {a:>4d} + {b:>4d}k")
             a, b = collatze_vector(a, b)
-        print(f"  {a:>4d} + {b:>4d}k")
+        print(f" {a:>{maxResidueWidth}d} + {b:>{maxModulusWidth}d}k ", end="")
 
         # Report conclusions, checking each of the loop-exiting conditions
         if (b < N):
-            print(f"  Decreases       ((b={b}) <(N={N}))")
+            print(f"  Decreases       ((b={b:>{maxModulusWidth}d}) < (N={N}))")
         elif ((b % 2) == 1):
-            print(f"  Needs splitting ((b={b} is odd))")
+            print(f"  Needs splitting ((b={b:>{maxModulusWidth}d} is odd))")
             needsSplitting.append(A)
         elif (iters >= maxIters):
             print(f"  Needs more iters (current maxIters is {maxIters})")
@@ -48,18 +53,26 @@ def collatzModN(N, residueList):
     if (len(tooManyIters) > 0):
         print()
         print(f"There are entries that required more iters than were allowed (maxIters was {maxIters})")
+        print("  Exiting.")
         return
     else:
         return needsSplitting
 
 # Main routine
 N = 2
-needsSplitting = collatzModN(N, range(N))
-while needsSplitting:
+nLog2 = 1
+maxResidueListLenWidth=4
+residueList=range(N)
+while residueList:
     print("-"*50)
-    print(f"needsSplitting for N = {N}: {needsSplitting}")
-    splitInput = sorted([ x for xx in [[ a, a + N ] for a in needsSplitting] for x in xx])
+    print(f"{len(residueList):>{maxResidueListLenWidth}d} residues to check          for N = {N}: {residueList}")
+    needsSplitting = collatzModN(N, residueList)
+    if (needsSplitting):
+        print(f"{len(needsSplitting):>{maxResidueListLenWidth}d} residues needing splitting for N = {N}: {needsSplitting}")
+        if (len(needsSplitting) == len(residueList)):
+            print(f"  NO ADDITIONAL PROGRESS FOR N = {N} (2 ** {nLog2})")
+        residueList = sorted([ x for xx in [[ a, a + N ] for a in needsSplitting] for x in xx])
+    else:
+        residueList = list()
     N *= 2
-    print(f"splitInput     for N = {N}: {splitInput}")
-    needsSplitting = collatzModN(N, splitInput)
     nLog2 += 1
