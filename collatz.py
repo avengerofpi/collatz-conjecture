e9c4c3c6ddbb2183adc2ee87ef16f0d71887cbe2
Cory Washington <tbd>
2022-07-24 12:08:36 -0400

    Stop tracking alreadySeen values completely
    I intend to try switching to a SQL database-based method for tracking
    values that have already been seen. Will very likely be slower, but will
    reduce RAM consumption I highly expect. And if I can be careful about
    the values I bother to store (like only n such that n = 1 (mod 2**10)),
    then maybe I can reduce the aniticipated slowdown a lot.

--- collatz.py
+++ collatz.py
@@ -13,7 +13,6 @@ def collatz(n):
 def gen_collatz(n):
     nList = [n]
     isLoop = False
-    #while ((n > 1) and not (n in alreadySeen)):
     while (n > 1):
         n = collatz(n)
         nList.append(n)
@@ -30,7 +29,6 @@ minI=1
 maxI=30
 
 # Help to stop early when we've seen a degenerate case before
-alreadySeen = set()
 countMap = { 1: 1 }
 nToListMap = { 1: [] }
 
@@ -49,18 +47,7 @@ for i in range(minI, maxI):
         break
 
     lastElement = nList[-1]
-    # Subtract 1 for the overlap between lists
-    # Switch to keeping track of a subset of previously seen values, to avoid memory overload
     print(f"{i:>{widthLargestStart}} -> {n:>{widthLargestModifiedStart}d} ({len(nList):>6} steps before a short-circuit route was encountered: {lastElement})")
-    alreadySeen.add(n)
     nListLen = len(nList)
-    if (nListLen >= 100):
-        decimateStepSize = (nListLen - (nListLen % 10)) // 10
-        for ii in range(0, nListLen, decimateStepSize):
-            a = nList[ii]
-            #print(a)
-            alreadySeen.add(nList[ii])
-    for ii in range(len(nList)):
-        nn = nList[ii]
-        alreadySeen.add(nn)
+
 
