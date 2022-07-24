9e21b5968696d67ca10c081861a4e704111d6471
Cory Washington <tbd>
2022-07-24 12:25:57 -0400

    plotting some data
    Path length, which tends to be linear with sporadic jumps.
    
    Path length - seed value/starting value bitLenth, which tends to be
      constant with sporadic jumps.

--- collatz.py
+++ collatz.py
@@ -31,13 +31,15 @@ maxI=30
 # Help to stop early when we've seen a degenerate case before
 countMap = { 1: 1 }
 nToListMap = { 1: [] }
+nList_lengthList = list()
 
 # Printing-help vars
 widthLargestStart =         int(log10(maxI)) + 1
 widthLargestCount =         int(log10(maxI)) + 2
 widthLargestModifiedStart = int(log10(modifyStart(maxI))) + 1
 
-for i in range(minI, maxI):
+iRange = range(minI, maxI + 1)
+for i in iRange:
     n = modifyStart(i)
     nList, isLoop = gen_collatz(n)
 
@@ -49,5 +51,43 @@ for i in range(minI, maxI):
     lastElement = nList[-1]
     print(f"{i:>{widthLargestStart}} -> {n:>{widthLargestModifiedStart}d} ({len(nList):>6} steps before a short-circuit route was encountered: {lastElement})")
     nListLen = len(nList)
+    nList_lengthList.append(nListLen)
 
+# Graph
+# Scatterplot (see https://matplotlib.org/stable/plot_types/index
+# for a list of matplotlib plot types
+import matplotlib.pyplot as plt
+import numpy as np
+import inspect
+import re
+
+plt.style.use('_mpl-gallery')
+
+# make the data
+x = iRange
+y = nList_lengthList
+yFlattened = [ y[i] - x[i] for i in range(len(x))]
+print(f"x: {x}")
+print(f"y: {y}")
+# size and color:
+#sizes = np.random.uniform(15, 80, len(x))
+#colors = np.random.uniform(15, 80, len(x))
+plt.plot(x, y, 'b.')
+plt.plot(x, yFlattened, 'r.')
+
+plt.xlabel(f"Lengths of Collatz paths for {re.sub('.*return |def .*', '', inspect.getsource(modifyStart), re.DOTALL)}")
+
+plt.title(f"{inspect.getsource(modifyStart)}")
+plt.title('hello world', loc='center')
+plt.subplots_adjust(left=0.05, bottom=0.10, right=0.97, top=0.95, wspace=None, hspace=None)
+
+# plot
+#fig, ax = plt.subplots()
+#
+#ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
+#
+#ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+#       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
 
